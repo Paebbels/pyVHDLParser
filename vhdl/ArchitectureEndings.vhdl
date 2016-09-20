ae571e44c0f3a9490d973f122824f6b8f5e44de8
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-20 17:44:07 +0200

    Added VHDL test files.

---
+++ vhdl/ArchitectureEndings.vhdl
@@ -0,0 +1,91 @@
+architecture
+rtl0
+of
+myEntity0
+is
+end
+;
+
+architecture 
+rtl00 
+of 
+myEntity00 
+is 
+end 
+; 
+
+ architecture
+ rtl000
+ of
+ myEntity000
+ is
+ end
+ ;
+
+architecture    -- comment0
+rtl1            -- comment1
+of              -- comment2
+myEntity1       -- comment3
+is              -- comment4
+end             -- comment5
+;               -- comment6
+
+architecture-- comment0
+rtl2-- comment1
+of-- comment2
+myEntity2-- comment3
+is-- comment4
+end-- comment5
+;-- comment6
+
+architecture    /* comment0 */
+rtl3            /* comment1 */
+of              /* comment2 */
+myEntity3       /* comment3 */
+is              /* comment4 */
+end             /* comment5 */
+;               /* comment6 */
+
+architecture/* comment0 */
+rtl4/* comment1 */
+of/* comment2 */
+myEntity4/* comment3 */
+is/* comment4 */
+end/* comment5 */
+;/* comment6 */
+
+architecture rtl5 of myEntity5 is end;
+architecture rtl6 of myEntity6 is end architecture;
+architecture rtl7 of myEntity7 is end rtl7;
+architecture rtl8 of myEntity8 is end architecture rtl8;
+architecture rtl9 of myEntity9 is end architecture rtl9 ;
+
+architecture	rtl10	of	myEntity10	is	end;
+architecture	rtl11	of	myEntity11	is	end	architecture;
+architecture	rtl12	of	myEntity12	is	end	rtl12;
+architecture	rtl13	of	myEntity13	is	end	architecture	rtl13;
+architecture	rtl14	of	myEntity14	is	end	architecture	rtl14 ;
+
+architecture rtl15 of myEntity15 is 
+end;
+
+architecture rtl16 of myEntity16 is
+end ;
+
+architecture rtl17 of myEntity17 is
+end architecture;
+
+architecture rtl18 of myEntity18 is
+end architecture ;
+
+architecture rtl19 of myEntity19 is
+end rtl19;
+
+architecture rtl20 of myEntity20 is
+end rtl20 ;
+
+architecture rtl21 of myEntity21 is
+end architecture rtl21;
+
+architecture rtl23 of myEntity23 is
+end architecture rtl23 ;
---
+++ vhdl/EntityEndings.vhdl
@@ -0,0 +1,65 @@
+entity
+myEntity0
+is
+end
+;
+
+entity    -- comment0
+myEntity1 -- comment1
+is        -- comment2
+end       -- comment3
+;         -- comment4
+
+entity-- comment0
+myEntity2-- comment1
+is-- comment2
+end-- comment3
+;-- comment4
+
+entity    /* comment0 */
+myEntity1 /* comment1 */
+is        /* comment2 */
+end       /* comment3 */
+;         /* comment4 */
+
+entity/* comment0 */
+myEntity4/* comment1 */
+is/* comment2 */
+end/* comment3 */
+;/* comment4 */
+
+entity myEntity5 is end;
+entity myEntity6 is end entity;
+entity myEntity7 is end myEntity7;
+entity myEntity8 is end entity myEntity8;
+entity myEntity9 is end entity myEntity9 ;
+
+entity	myEntity10	is	end;
+entity	myEntity11	is	end	entity;
+entity	myEntity12	is	end	myEntity12;
+entity	myEntity13	is	end	entity	myEntity13;
+entity	myEntity14	is	end	entity	myEntity14 ;
+
+entity myEntity15 is 
+end;
+
+entity myEntity16 is
+end ;
+
+entity myEntity17 is
+end entity;
+
+entity myEntity18 is
+end entity ;
+
+entity myEntity19 is
+end myEntity19;
+
+entity myEntity20 is
+end myEntity20 ;
+
+entity myEntity21 is
+end entity myEntity22;
+
+entity myEntity23 is
+end entity myEntity23 ;
