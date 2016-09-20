219b1e097fe0a5120322bd730df3c213746cb84a
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-21 01:56:07 +0200

    Added tests for LIBRARY and USE.

---
+++ vhdl/Library.vhdl
@@ -0,0 +1,25 @@
+library library0;
+
+library library1 ;
+
+library
+	library2
+		;
+
+library 
+	library3 
+		; 
+
+library library4;  -- comment
+
+library -- comment1
+	library5  -- comment2
+; -- comment3
+
+library-- comment
+	library6--
+	;-- 
+
+library
+library7
+;
---
+++ vhdl/Use.vhdl
@@ -0,0 +1,9 @@
+use lib0.pkg0.all;
+
+use lib1.pkg1.all ;
+
+use lib2.pkg2.all; -- comment
+
+use		lib3.pkg3.all		;
+
+use lib4.pkg4.const4;
