d8ef51bd1c9ac1eb4c3fba37d1447b06d454f949
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-21 01:48:17 +0200

    Added simple example.

---
+++ vhdl/Simple_1.vhdl
@@ -0,0 +1,21 @@
+-- Copryright 2016
+library IEEE;
+use     IEEE.std_logic_1164.all;
+use			IEEE.numeric_std.all;
+
+entity myEntity is
+	generic (
+		BITS : positive := 8
+	);
+	port (
+		Clock   : in  std_logic;
+		Reset   : in	std_logic;
+		Output	: out	std_logic_vector(BITS - 1 downto 0)
+	);
+end entity;
+
+architecture rtl of myEntity is
+
+begin
+
+end architecture;
