0d51b5053d8b8b323587a5b4caca202406224a4e
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-21 05:41:00 +0200

    Added a basic graph data structure.

---
+++ src/Graph/Graph.py
@@ -0,0 +1,103 @@
+# EMACS settings: -*-	tab-width: 2; indent-tabs-mode: t; python-indent-offset: 2 -*-
+# vim: tabstop=2:shiftwidth=2:noexpandtab
+# kate: tab-width 2; replace-tabs off; indent-width 2;
+# ==============================================================================
+# Authors:            Patrick Lehmann
+#
+# Python functions:   A streaming VHDL parser
+#
+# Description:
+# ------------------------------------
+#		TODO:
+#
+# License:
+# ==============================================================================
+# Copyright 2007-2016 Patrick Lehmann - Dresden, Germany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+#
+from enum import Enum
+
+
+class NodeColors(Enum):
+	Black = 0
+	Gray = 1
+	White = 2
+
+
+class EdgeKinds(Enum):
+	Unknown =  0
+	Forward =  1
+	Backward = 2
+	Sideward = 3
+
+
+class Graph:
+	def __init__(self):
+		self._nodes =           []
+		self._roots =           []
+		self._forwardEdges =    []
+		self._backwardEdges =   []
+		self._sidewardEdges =   []
+
+	def AddNode(self, node):
+		node.Graph =    self
+		self._nodes.append(node)
+
+	def __colorize(self, node):
+		if (node.Color is NodeColors.Black):
+			node.Color = NodeColors.Gray
+			for edge in node._edges:
+				edge.EdgeKind = self.__colorize(edge.End)
+				if (edge.EdgeKind is EdgeKinds.Forward):
+					self._forwardEdges.append(edge)
+				elif (edge.EdgeKind is EdgeKinds.Backward):
+					self._backwardEdges.append(edge)
+				elif (edge.EdgeKind is EdgeKinds.Sideward):
+					self._sidewardEdges.append(edge)
+
+			return EdgeKinds.Forward
+		elif (node.Color is NodeColors.Gray):
+			return EdgeKinds.Backward
+		elif (node.Color is NodeColors.White):
+			return  EdgeKinds.Sideward
+
+	def Colorize(self):
+		for startNode in self._nodes:
+			if startNode.Color is not NodeColors.White:
+				self.__colorize(startNode)
+
+	def ClearColors(self):
+		for node in self._nodes:
+			node.Color = NodeColors.Black
+
+
+class Node:
+	def __init__(self):
+		self.Graph =    None
+		self._edges =   []
+		self.Visited =  False
+		self.Color =    NodeColors.Black
+
+	def DependsOn(self, node):
+		edge = Edge(self.Graph, self, node)
+		self._edges.append(edge)
+
+
+class Edge:
+	def __init__(self, graph, start, end):
+		self.Graph =    graph
+		self.EdgeKind = EdgeKinds.Unknown
+		self.Start =    start
+		self.End =      end
