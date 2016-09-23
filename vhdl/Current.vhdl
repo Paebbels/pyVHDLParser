bb8df765b80575dc6141dde9ac7b955837f009ce
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-23 04:46:59 +0200

    Changed current test example.

--- vhdl/Current.vhdl
+++ vhdl/Current.vhdl
@@ -1,26 +1,30 @@
-architecture
-rtl0
-of
-myEntity0
-is
-begin
-end
-;
 
-architecture 
-rtl00 
-of 
-myEntity00 
-is 
-begin 
-end 
-; 
+entity    /* comment0 */
+myEntity1 /* comment1 */
+is        /* comment2 */
+end       /* comment3 */
+;         /* comment4 */
 
- architecture
- rtl000
- of
- myEntity000
- is
- begin
- end
- ;
+entity/* comment0 */
+myEntity4/* comment1 */
+is/* comment2 */
+end/* comment3 */
+;/* comment4 */
+
+architecture    /* comment0 */
+rtl3            /* comment1 */
+of              /* comment2 */
+myEntity3       /* comment3 */
+is              /* comment4 */
+begin           /* comment5 */
+end             /* comment6 */
+;               /* comment7 */
+
+architecture/* comment0 */
+rtl4/* comment1 */
+of/* comment2 */
+myEntity4/* comment3 */
+is/* comment4 */
+begin/* comment5 */
+end/* comment6 */
+;/* comment7 */
