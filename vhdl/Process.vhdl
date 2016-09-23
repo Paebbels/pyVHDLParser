0a5d8e4841ac110a6d64b3adf9de8c290f23797a
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-24 00:17:36 +0200

    Added test cases for processes.

---
+++ vhdl/Process.vhdl
@@ -0,0 +1,39 @@
+architecture rtl of myEntity is
+begin
+	process(all)
+	begin
+	end process;
+
+	process(Clock)
+	begin
+	end process;
+
+	process(Clock) begin
+	end process;
+
+	process(Clock)begin
+	end process;
+	
+	process (Clock, Reset)
+	begin
+	end process;
+
+	process(Clock) is
+	begin
+	end process;
+
+	process(Clock) is begin
+	end process;
+
+	proc:process(Clock)
+	begin
+	end process;
+
+	proc: process(Clock)
+	begin
+	end process;
+
+	proc : process(Clock)
+	begin
+	end process;
+end architecture;
