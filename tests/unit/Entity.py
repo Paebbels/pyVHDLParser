86fd2bcbee0e0577231f8b6e7bb0934ccf8ced0a
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-01-11 04:14:16 +0100

    Added first ideas on how to write parser tests.

---
+++ tests/unit/Entity.py
@@ -0,0 +1,262 @@
+from pyVHDLParser.Token             import WordToken, StartOfDocumentToken, SpaceToken, CharacterToken, EndOfDocumentToken
+from pyVHDLParser.Blocks            import StartOfDocumentBlock, EndOfDocumentBlock, BlockParserException
+from pyVHDLParser.Blocks.Common     import WhitespaceBlock
+from pyVHDLParser.Blocks.Structural import Entity
+
+from tests.unit                     import StreamingTests, Result
+
+
+TESTCASES = [
+	{ 'name': "Simple:Entity: one line; only end",
+		'code': "entity e is end;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),           #
+				(Entity.NameBlock,     "entity e is"),  # entity e is
+				(WhitespaceBlock,      " "),            #
+				(Entity.EndBlock,      "end;"),         # end;
+				(EndOfDocumentBlock,   None)            #
+			],
+		  'result': Result.Pass
+	  }
+	},{
+		'name': "Simple:Entity: one line; ends with keyword",
+		'code': "entity e is end entity;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),           #
+				(Entity.NameBlock,     "entity e is"),  # entity e is
+				(WhitespaceBlock,      " "),            #
+				(Entity.EndBlock,      "end entity;"),  # end entity;
+				(EndOfDocumentBlock,   None)            #
+			],
+		  'result': Result.Pass
+		}
+	},{
+		'name': "Simple:Entity: one line; ends with name",
+		'code': "entity e is end e;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),           #
+				(Entity.NameBlock,     "entity e is"),  # entity e is
+				(WhitespaceBlock,      " "),            #
+				(Entity.EndBlock,      "end e;"),       # end e;
+				(EndOfDocumentBlock,   None)            #
+			],
+		  'result': Result.Pass
+		}
+	},{
+		'name': "Simple:Entity: one line; ends with keyword and name",
+		'code': "entity e is end entity e;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),             #
+				(Entity.NameBlock,     "entity e is"),    # entity e is
+				(WhitespaceBlock,      " "),              #
+				(Entity.EndBlock,      "end entity e;"),  # end entity e;
+				(EndOfDocumentBlock,   None)              #
+			],
+		  'result': Result.Pass
+		}
+	},{
+		'name': "Simple:Entity: one line; no name; ends with keyword and name",
+		'code': "entity is end entity e;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),             #
+				(Entity.NameBlock,     "entity is"),      # entity is
+				(WhitespaceBlock,      " "),              #
+				(Entity.EndBlock,      "end entity e;"),  # end entity e;
+				(EndOfDocumentBlock,   None)              #
+			],
+		  'result':     Result.Fail,
+		  'Exception':  BlockParserException
+		}
+	},{
+		'name': "Simple:Entity: one line; no is; ends with keyword and name",
+		'code': "entity e end entity e;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),             #
+				(Entity.NameBlock,     "entity e"),       # entity e
+				(WhitespaceBlock,      " "),              #
+				(Entity.EndBlock,      "end entity e;"),  # end entity e;
+				(EndOfDocumentBlock,   None)              #
+			],
+		  'result':     Result.Fail,
+		  'Exception':  BlockParserException
+		}
+	},{
+		'name': "Simple:Entity: one line; no end; ends with keyword and name",
+		'code': "entity e is entity e;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),             #
+				(Entity.NameBlock,     "entity e is"),    # entity e is
+				(WhitespaceBlock,      " "),              #
+				(Entity.EndBlock,      "entity e;"),      # entity e;
+				(EndOfDocumentBlock,   None)              #
+			],
+		  'result':     Result.Fail,
+		  'Exception':  BlockParserException
+		}
+	},{
+		'name': "Simple:Entity: one line; ends with keyword and name; but wrong name",
+		'code': "entity e is end entity a;",
+		'tokenstream': {
+			'tokens': [
+				(StartOfDocumentToken, None),      #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "e"),       # e
+				(SpaceToken,           " "),       #
+				(WordToken,            "is"),      # is
+				(SpaceToken,           " "),       #
+				(WordToken,            "end"),     # end
+				(SpaceToken,           " "),       #
+				(WordToken,            "entity"),  # entity
+				(SpaceToken,           " "),       #
+				(WordToken,            "a"),       # a
+				(CharacterToken,       ";"),       # ;
+				(EndOfDocumentToken,   None)       #
+			],
+			'result': Result.Pass
+		},
+	  'blockstream': {
+			'blocks': [
+				(StartOfDocumentBlock, None),             #
+				(Entity.NameBlock,     "entity e is"),    # entity e is
+				(WhitespaceBlock,      " "),              #
+				(Entity.EndBlock,      "end entity a;"),  # end entity a;
+				(EndOfDocumentBlock,   None)              #
+			],
+		  'result':     Result.Pass
+		}
+	}
+]
+
+
+class SimpleEntities(StreamingTests):
+	_TESTCASES = TESTCASES
+
+	def setUp(self):
+		pass
---
+++ tests/unit/__init__.py
@@ -0,0 +1,142 @@
+from unittest                   import TestCase
+
+from flags                      import Flags
+
+from pyVHDLParser.Token         import StartOfDocumentToken, EndOfDocumentToken, Token
+from pyVHDLParser.Token.Parser  import Tokenizer
+from pyVHDLParser.Blocks        import StartOfDocumentBlock, EndOfDocumentBlock, TokenToBlockParser, MetaBlock, Block
+
+
+def setUpModule():
+	for block in MetaBlock.BLOCKS:
+		try:
+			block.__cls_init__()
+		except AttributeError:
+			pass
+
+
+class Result(Flags):
+	Pass = 1,
+	Fail = 2
+
+
+class StreamingTests(TestCase):
+	_TESTCASES = []
+
+	def setUp(self):
+		self.skipTest()
+
+	def test_TokenSequence(self) -> None:
+		for test in self._TESTCASES:
+			with self.subTest(msg=test['name']):
+				tokenStream = Tokenizer.GetVHDLTokenizer(test['code'])
+
+				for pair in zip(tokenStream, test['tokenstream']['tokens']):
+					self.assertIsInstance(pair[0], pair[1][0])
+					if pair[1][1] is not None:
+						self.assertTrue(pair[0] == pair[1][1])
+
+	def test_TokenLinking(self) -> None:
+		for test in self._TESTCASES:
+			with self.subTest(msg=test['name']):
+				tokenStream = Tokenizer.GetVHDLTokenizer(test['code'])
+
+				tokenIterator = iter(tokenStream)
+				startToken = next(tokenIterator)
+
+				self.assertIsInstance(startToken, StartOfDocumentToken,
+				                      msg="First token is not StartOfDocumentToken: {token}".format(token=startToken))
+				self.assertIsNone(startToken.PreviousToken, msg="First token has no open start.")
+
+				lastToken: Token = startToken
+				endToken: Token = None
+
+				for token in tokenIterator:
+					if isinstance(token, EndOfDocumentToken):
+						endToken = token
+						break
+
+					self.assertEqual(lastToken.NextToken, token,
+					                 msg="Last token is not connected to the current token: {token}".format(token=token))
+					self.assertEqual(lastToken, token.PreviousToken,
+					                 msg="Current token is not connected to lastToken: {token}".format(token=token))
+
+					lastToken = token
+				else:
+					self.fail(msg="No EndOfDocumentToken found.")
+
+				self.assertIsInstance(endToken, EndOfDocumentToken,
+				                      msg="End token is not EndOfDocumentToken: {token}".format(token=endToken))
+				self.assertEqual(lastToken.NextToken, endToken,
+				                 msg="Last token is not connected to the end token: {token}".format(token=token))
+				self.assertEqual(lastToken, endToken.PreviousToken,
+				                 msg="End token is not connected to lastToken: {token}".format(token=token))
+				self.assertIsNone(endToken.NextToken, msg="End token has no open end: {token}".format(token=endToken.NextToken))
+
+	def test_BlockSequence(self) -> None:
+		for test in self._TESTCASES:
+			with self.subTest(msg=test['name']):
+				tokenStream = Tokenizer.GetVHDLTokenizer(test['code'])
+				blockStream = TokenToBlockParser.Transform(tokenStream)
+
+				for pair in zip(blockStream, test['blockstream']['blocks']):
+					self.assertIsInstance(pair[0], pair[1][0])
+
+	def test_BlockLinking(self) -> None:
+		for test in self._TESTCASES:
+			with self.subTest(msg=test['name']):
+				tokenStream = Tokenizer.GetVHDLTokenizer(test['code'])
+				blockStream = TokenToBlockParser.Transform(tokenStream)
+
+				blockIterator = iter(blockStream)
+				firstBlock = next(blockIterator)
+
+				self.assertIsInstance(firstBlock, StartOfDocumentBlock,
+				                      msg="First block is not StartOfDocumentBlock: {block}".format(block=firstBlock))
+
+				startToken = firstBlock.StartToken
+				self.assertIsInstance(startToken, StartOfDocumentToken,
+				                      msg="First token is not StartOfDocumentToken: {token}".format(token=startToken))
+
+				lastBlock: Block = firstBlock
+				endBlock: Block = None
+				lastToken: Token = startToken
+
+				for block in blockIterator:
+					if isinstance(block, EndOfDocumentBlock):
+						endBlock = block
+						break
+
+					# Block checks
+					self.assertEqual(lastBlock.NextBlock, block,
+					                 msg="Last block is not connected to the current block: {block}".format(block=block))
+					self.assertEqual(lastBlock, block.PreviousBlock,
+					                 msg="Current block is not connected to last block: {block}".format(block=block))
+
+					# Token checks
+					tokenIterator = iter(block)
+
+					for token in tokenIterator:
+						self.assertIsNotNone(token.NextToken, msg="Token has an open end (token).".format(token=token.NextToken))
+						self.assertEqual(lastToken.NextToken, token, msg="Last token is not connected to the current token.")
+						self.assertIsNotNone(token.PreviousToken, msg="Token has an open end (PreviousToken).")
+						self.assertEqual(token.PreviousToken, lastToken, msg="Current token is not connected to lastToken.")
+
+						lastToken = token
+
+					lastBlock = block
+				else:
+					self.fail(msg="No EndOfDocumentBlock found.")
+
+				# Block checks
+				self.assertIsInstance(endBlock, EndOfDocumentBlock,
+				                      msg="End block is not EndOfDocumentblock: {token}".format(token=endBlock))
+				self.assertIsInstance(endBlock.EndToken, EndOfDocumentToken,
+				                      msg="End block's token is not EndOfDocumentToken: {token}".format(
+					                      token=endBlock.EndToken))
+
+				# Token checks
+				self.assertEqual(lastToken.NextToken, endBlock.EndToken, msg="Last token is not connected to the end token.")
+				self.assertEqual(lastToken, endBlock.EndToken.PreviousToken, msg="End token is not connected to lastToken.")
+				self.assertIsNone(endBlock.EndToken.NextToken,
+				                  msg="End token has no open end: {token}".format(token=endBlock.EndToken.NextToken))
