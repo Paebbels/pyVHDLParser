eb719ce513746df2a984c108b50fefdeaa523f24
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-01-14 20:49:31 +0100

    Bump version to 0.4.4.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
 
 setuptools.setup(
 	name=projectName,
-	version="0.4.3",
+	version="0.4.4",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",
